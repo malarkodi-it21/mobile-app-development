373c8d7cfede1389d39f3203aced017bdab32e91
malarkodi-it21 <95203223+malarkodi-it21@users.noreply.github.com>
2022-10-11 16:25:17 +0530

    Create MainActivity3.java

---
+++ MainActivity3.java
@@ -0,0 +1,30 @@
+package com.example.myapp;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.myapp.MainActivity4;
+import com.google.android.material.textfield.TextInputLayout;
+
+public class MainActivity3 extends AppCompatActivity {
+    TextInputLayout email, Password;
+    TextView bt_button;
+    public TextView button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = (TextView) findViewById(R.id.button5);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(com.example.myapp.MainActivity3.this, MainActivity4.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
